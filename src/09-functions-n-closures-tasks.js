42a0cfc1a72d6160f6ecf8c3d3380c12f8674891
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2019-12-20 17:30:59 +0300

    finish core-js-101 task

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -92,8 +92,12 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+  return () => {
+    if (cache === undefined) cache = func();
+    return cache;
+  };
 }
 
 
@@ -112,8 +116,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        i += 1;
+        i -= 1;
+      }
+    }
+    return '';
+  };
 }
 
 
@@ -140,8 +154,20 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    let logs = '';
+    args.forEach((item) => {
+      logs += `${JSON.stringify(item)},`;
+    });
+    if (logs[logs.length - 1] === ',') logs = logs.slice(0, logs.length - 1);
+    const start = `${func.name}(${logs}) starts`;
+    logFunc(start);
+    const result = func(...args);
+    const end = `${func.name}(${logs}) ends`;
+    logFunc(end);
+    return result;
+  };
 }
 
 
@@ -158,8 +184,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...args1, ...args2);
 }
 
 
@@ -180,8 +206,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom - 1;
+  return () => {
+    count += 1;
+    return count;
+  };
 }
 
 
